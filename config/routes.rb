e1f0e7b19c9cfbcb4d0481f7705404759bdfdf2e
nicaa0695 <nicaa0695@gmail.com>
2020-02-27 11:23:17 -0700

    added dashboard route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'dashboard/index'
+  get "/dashboard" => 'dashboard#index', as: :dashboard 
   get 'dashboard/properties'
   get 'dashboard/reports'
+
   resources :properties
   devise_for :accounts
   root 'public#main'
