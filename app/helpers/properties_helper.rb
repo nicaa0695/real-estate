049272a3c5b55e9fa5a104a5903150f0ab4d8d68
nicaa0695 <nicaa0695@gmail.com>
2020-03-24 15:17:51 -0600

    added main method in publicController for latest properties

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -1,6 +1,6 @@
 module PropertiesHelper
-    def property_thumbnail
-        img = property.photo.present ? property.photo.thumb.url : "placeholder.jpg"
+    def property_thumbnail property
+        img = property.photo.present? ? property.photo.thumb.url : "placeholder.png"
         image_tag img, class: "property-thumb"
     end
 end
