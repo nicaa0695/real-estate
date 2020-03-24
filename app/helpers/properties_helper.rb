71c2bdb8d133cccdeee478e28c34381842ef3578
nicaa0695 <nicaa0695@gmail.com>
2020-03-24 15:09:27 -0600

    created helper method for lastest_properties to show up on homepage

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -1,2 +1,6 @@
 module PropertiesHelper
+    def property_thumbnail
+        img = property.photo.present ? property.photo.thumb.url : "placeholder.jpg"
+        image_tag img, class: "property-thumb"
+    end
 end
