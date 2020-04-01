ced601b0365fa748c45570bbddab8540fc78f520
nicaa0695 <nicaa0695@gmail.com>
2020-04-01 17:08:52 -0600

    added before action and method for sidebar

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,13 @@
 class DashboardController < ApplicationController
+  before_action :set_sidebar
+
   def index
   end
 
-  def properties
-  end
+  private
 
-  def reports
-  end
+  def set_sidebar
+    @show_sidebar = true
+  end 
+  
 end
