574a94d4addec0b2575616dd92c04e55c4802fa0
nicaa0695 <nicaa0695@gmail.com>
2020-04-07 11:21:52 -0600

    debugging

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,6 +1,6 @@
 class Property < ApplicationRecord
     belongs_to :account
-    mount_uploader :photo, PhotoUploader
+    # mount_uploader :photo, PhotoUploader
   
     validates :name, presence: true, length: {minimum: 3, maximum: 50}
     validates :address, presence: true, length: {minimum: 3, maximum: 100}
