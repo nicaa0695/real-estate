f43cc07bb7deca63de8c6a07980ced8a303053d8
nicaa0695 <nicaa0695@gmail.com>
2020-04-17 13:51:24 -0600

    bug fix

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -3,4 +3,9 @@ module PropertiesHelper
         img = property.photo.present? ? property.photo.thumb.url : "placeholder.png"
         image_tag img, class: "property-thumb"
     end
+
+    def property_photo property
+        img = property.photo.present? ? property.photo.url : "placeholder.png"
+        image_tag img, class: "property-photo"
+    end
 end
