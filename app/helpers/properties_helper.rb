38e79821701c343f51e244db9aaf486bfe15da3f
nicaa0695 <nicaa0695@gmail.com>
2020-04-22 14:07:40 -0600

    style changes for photos of home

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -4,8 +4,7 @@ module PropertiesHelper
         image_tag img, class: "property-thumb"
     end
 
-    def property_photo property
-        img = property.photo.present? ? property.photo.url : "placeholder.png"
-        image_tag img, class: "property-photo"
+    def property_photo_url property
+        property.photo.present? ? property.photo.url : asset_url("placeholder.png")
     end
 end
